1cd3e4ce4534454e549109d0bc598a75a657c8d7
jliocsar <juliocesarmfo@gmail.com>
2023-11-08 00:08:29 -0300

    wip normalize

--- server/layout.js
+++ server/layout.js
@@ -30,7 +30,7 @@ export function Layout(/** @type {TProps} */ props) {
         <link rel="stylesheet" href="static/style.css" />
         <title>${props.title || 'Hono'}</title>
       </head>
-      <body class="m-0">
+      <body class="my-0">
         ${props.content}
       </body>
     </html>
